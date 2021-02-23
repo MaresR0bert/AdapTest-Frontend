5e4652ac59064072f9eadc58df3089f73eecf9d1
MaresR0bert <robi_mares@yahoo.com>
2021-02-23 22:44:25 +0200

    comment prototype

--- src/App.js
+++ src/App.js
@@ -6,6 +6,37 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 function App() {
   return (
     <MainPage></MainPage>
+    /* 
+    constructor(props){
+      super(props)
+      this.state = {
+        status:0
+      }
+    }
+    
+    render(){
+      if(this.state.status === 1){
+        return <MainPageTeacher />
+      }else if(this.state.status === 2) return <MainPageStudent />
+
+      return(
+        <div>
+          <input>username</input>
+          <button onClick={()=>{
+            this.setState({
+              status:2
+            })
+          }}>Log In As Student</button>
+
+          <button onClick={()=>{
+            this.setState({
+              status:1
+            })
+          }}>Log In As Teacher</button>
+        </div>
+      )
+    }
+    */
   );
 }
 
