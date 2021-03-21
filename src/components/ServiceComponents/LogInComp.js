2b5c762c9ef192b94d4853e2f13585c58dc199b7
MaresR0bert <robi_mares@yahoo.com>
2021-03-22 00:16:25 +0200

    added login screen

--- src/components/ServiceComponents/LogInComp.js
+++ src/components/ServiceComponents/LogInComp.js
@@ -10,9 +10,9 @@ export default class LogIn extends Component {
     }
 
     render() {
-        if (this.state.status === true) window.location = '/teacher/'; 
-        else if(this.state.status === false) window.location = '/student/'
-        else if(this.state.status === 'register') window.location ='/register/'
+        if (this.state.status === true) window.location = '/teacher/';
+        else if (this.state.status === false) window.location = '/student/'
+        else if (this.state.status === 'register') window.location = '/register/'
 
         return (
             <div className='container'>
@@ -21,14 +21,31 @@ export default class LogIn extends Component {
                         status: true
                     })
                 }}>Teacher</button>
-                <br/>
+                <br />
                 <button onClick={() => {
                     this.setState({
                         status: false
                     })
                 }}>Student</button>
                 <br />
-                <button onClick={()=>{
+
+                <h1 className='text-center'>Welcome to Adaptest!</h1>
+                <h1 className='text-center'>The free adaptive testing platform</h1>
+                <br />
+                <h2>Log in</h2>
+                <br />
+                <form>
+                    <h5>Username:</h5>
+                    <input type='text' className='form-control'></input>
+                    <br />
+                    <h5>Password:</h5>
+                    <input type='password' className='form-control'></input>
+                    <br />
+                    <input type='submit' className='btn btn-dark btn-lg btn-block' value='Log in'></input>
+                </form>
+                <br />
+                <h2>Or register right here:</h2>
+                <button className='btn btn-dark' onClick={() => {
                     this.setState({
                         status: 'register'
                     })
